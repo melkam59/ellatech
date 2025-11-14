b4332e52941055a829a405083db3bcef98e1a17a
melkam59 <melkamuelias59@gmail.com>
2025-11-15 02:37:18 +0300

    Setup cache and interceptors in core module

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,7 +1,50 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import config from 'src/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TransformResponseinterceptor } from './interceptors/transform-response/transform-response.interceptor';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
+import { CacheService } from './cache/cache.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const username = configService.get<string>('redis.username');
+        const password = configService.get<string>('redis.password');
+        return {
+          isGlobal: true,
+          store: await redisStore({
+            socket: {
+              host: configService.get<string>('redis.host'),
+              port: configService.get<number>('redis.port'),
+            },
+            ...(username && { username }),
+            ...(password && { password }),
+          }),
+          ttl: 10000,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [
+    CacheService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformResponseinterceptor,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
+  exports: [CacheService],
 })
 export class CoreModule {}
