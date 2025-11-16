e5369ff07f9a186098ed7351f55110d7bfbd89f2
melkam59 <melkamuelias59@gmail.com>
2025-11-16 22:44:18 +0300

    Update database module with TypeORM migrations

---
+++ src/core/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Global, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+        synchronize: false,
+        migrationsRun: true,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
